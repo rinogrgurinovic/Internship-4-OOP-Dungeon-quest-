5c482a42149930c2d70e8ae2b7987f8b4f8afdd1
rinogrgurinovic <grgurinovicrino@gmail.com>
2023-11-30 23:01:18 +0100

    Added class Hero

---
+++ DungeonQuest.Domain/Repositories/Hero.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonQuest.Domain.Repositories
+{
+    internal class Hero
+    {
+        public Hero(string name, int healthPoints, int experience, int damage)
+        {
+            Name = name;
+            HealthPoints = healthPoints;
+            Experience = experience;
+            Damage = damage;
+        }
+        public string Name { get; set; }
+        public int HealthPoints { get; set; }
+        public int Experience { get; set;}
+        public int Damage { get; set; }
+    }
+}
