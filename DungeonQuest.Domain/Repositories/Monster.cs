05e2b9b6f0b135ddc17a5df2ba951c44e35ad6b2
rinogrgurinovic <grgurinovicrino@gmail.com>
2023-12-01 23:18:38 +0100

    Added class Monster

---
+++ DungeonQuest.Domain/Repositories/Monster.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonQuest.Domain.Repositories
+{
+    internal class Monster
+    {
+        public Monster(int healthPoints, int damage)
+        {
+            HealthPoints = healthPoints;
+            Damage = damage;
+        }
+        public int HealthPoints { get; set; }
+        public int Damage { get; set; }
+    }
+}
