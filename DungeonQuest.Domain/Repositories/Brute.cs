0634d203325eae8d7b0b1c875b99d46eb94c75ca
rinogrgurinovic <grgurinovicrino@gmail.com>
2023-12-01 23:23:15 +0100

    Added class Brute

---
+++ DungeonQuest.Domain/Repositories/Brute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonQuest.Domain.Repositories
+{
+    internal class Brute : Monster
+    {
+        public Brute(int healthPoints, int damage, int trueDamage) : base(healthPoints, damage)
+        {
+            TrueDamage = trueDamage;
+        }
+        public int TrueDamage { get; set; }
+    }
+}
