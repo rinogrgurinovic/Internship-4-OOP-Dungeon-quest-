84a09d7016af90994fd9e4e299ac56862b4732df
rinogrgurinovic <grgurinovicrino@gmail.com>
2023-12-01 23:19:52 +0100

    Added class Goblin

---
+++ DungeonQuest.Domain/Repositories/Goblin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonQuest.Domain.Repositories
+{
+    internal class Goblin : Monster
+    {
+        public Goblin(int healthPoints, int damage) : base(healthPoints, damage)
+        {
+            
+        }
+    }
+}
