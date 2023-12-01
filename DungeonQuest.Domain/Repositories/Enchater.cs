f2f5d1b24ed23329c0f3b18ccd90ca75c1e99ce9
rinogrgurinovic <grgurinovicrino@gmail.com>
2023-12-01 17:43:49 +0100

    Added class Enchanter

---
+++ DungeonQuest.Domain/Repositories/Enchater.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonQuest.Domain.Repositories
+{
+    internal class Enchater : Hero
+    {
+        public Enchater(string name, int healthPoints, int experience, int damage, int mana) : base(name, healthPoints, experience, damage)
+        {
+            Mana = mana;
+        }
+        public int Mana { get; set; }
+    }
+}
