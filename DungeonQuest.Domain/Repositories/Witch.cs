95ee32f13970cbb6ad4502fdab8786ed83f1ff56
rinogrgurinovic <grgurinovicrino@gmail.com>
2023-12-01 23:24:48 +0100

    Added class Witch

---
+++ DungeonQuest.Domain/Repositories/Witch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonQuest.Domain.Repositories
+{
+    internal class Witch : Monster
+    {
+        public Witch(int healthPoints, int damage) : base(healthPoints, damage)
+        {
+            
+        }
+    }
+}
