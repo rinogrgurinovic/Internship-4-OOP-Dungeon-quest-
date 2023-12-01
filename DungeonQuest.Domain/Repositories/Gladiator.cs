f1196b576df7e8f83a742b3849207018e578fc77
rinogrgurinovic <grgurinovicrino@gmail.com>
2023-12-01 17:21:42 +0100

    Added class Gladiator

---
+++ DungeonQuest.Domain/Repositories/Gladiator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonQuest.Domain.Repositories
+{
+    internal class Gladiator : Hero
+    {
+        public Gladiator(string name, int healthPoints, int experience, int damage) : base(name, healthPoints, experience, damage)
+        {
+
+        }
+    }
+}
