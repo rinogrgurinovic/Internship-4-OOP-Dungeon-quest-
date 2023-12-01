fa0ea7f1f79246adfce63b039feb8f0e3027439a
rinogrgurinovic <grgurinovicrino@gmail.com>
2023-12-01 17:45:48 +0100

    Added class Marksman

---
+++ DungeonQuest.Domain/Repositories/Marksman.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonQuest.Domain.Repositories
+{
+    internal class Marksman : Hero
+    {
+        public Marksman(string name, int healthPoints, int experience, int damage, int criticalChance, int stunChance) : base(name, healthPoints, experience, damage)
+        {
+            CriticalChance = criticalChance;
+            StunChance = stunChance;
+        }
+        public int CriticalChance { get; set; }
+        public int StunChance { get; set; }
+    }
+}
